e524bd2c2eb8001500b3e7ef7d229747fdf6d6f3
DDI112 DDI112 <dorlasry71@gmail.com>
2023-04-02 15:11:47 +0300

    update requirements

--- movies.py
+++ movies.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import urllib.parse
-
+from PyMovieDb import IMDB
 def MovieDisplay():
-    pass
+
+    imdb = IMDB()
+    res = imdb.get_by_name('Titanic')
+    res = imdb.popular_movies(genre='Action', start_id=1, sort_by=None)
+    print(res)
+
+
+
+MovieDisplay()
