e2ab569590fcdb66aced4df302e4213833999fd1
DDI112 DDI112 <dorlasry71@gmail.com>
2023-04-02 12:28:35 +0300

    sections adding

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import urllib.parse
 from weather import WeatherDisplay
+from sports import SportsDisplay
+from movies import MovieDisplay
+from flight import FlightDisplay
 
 # menu style
 
@@ -9,13 +12,13 @@ while(1):
     print('1. movie section \n2. weather section \n3. flight section \n4. sport section \n8. exit')
     section = int(input('Choose a section: '))
     if (section == 1):
-        pass
+        MovieDisplay()
     elif (section == 2):
         WeatherDisplay()
     elif (section == 3):
-        pass
+        FlightDisplay()
     elif (section == 4):
-        pass
+        SportsDisplay()
     elif (section == 8):
         break
 
---
+++ sports.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.parse
+
+def SportsDisplay():
+    pass
